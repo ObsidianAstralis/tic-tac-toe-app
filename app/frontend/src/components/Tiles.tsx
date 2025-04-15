b8997dcd772e86ec532142ee535ccecac7f493a8
Obsidian Astralis <145642148+ObsidianAstralis@users.noreply.github.com>
2025-04-15 05:09:37 +0000

    ...

--- app/frontend/src/components/Tiles.tsx
+++ app/frontend/src/components/Tiles.tsx
@@ -11,7 +11,7 @@ const Tile: React.FC<SquareProps> = ({value, onClick, highlight}) => {
     <button 
       title='tile' 
       type="button" 
-      className={`size-28 border-2 border-gray-400 text-3xl font-bold bg-transparent shadow-md shadow-cyan-500
+      className={`size-28 border-4 border-gray-400 text-3xl font-bold bg-transparent shadow-md shadow-cyan-500
         hover:border-4 hover:border-indigo-600 hover:bg-gray-200 hover:text-black ${highlight ? "text-red-500 hover:text-red-500" : "text-white"}`}
       onClick={onClick}
     >
